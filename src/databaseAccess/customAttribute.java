7be8b3deb2ec36915859961bc1d38d20ec539251
Martin Stolarik <stolarik6@uniba.sk>
2020-05-11 16:44:28 +0200

    completed class 'customAttribute'

--- src/databaseAccess/customAttribute.java
+++ src/databaseAccess/customAttribute.java
@@ -1,4 +1,22 @@
 package databaseAccess;
 
+/**
+ *  Represents couple (attribute; value)
+ */
+
 public class customAttribute {
+    private final String name, value;
+
+    public customAttribute(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
